4f81786fba73b370a5703684d49cf239c0d834f7
Guilherme1993 <guinasc7@gmail.com>
2022-03-12 13:09:38 +0000

    revisão react

---
+++ topicos-especiais/src/componentes/FamiliaGomes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nome from './Nome'
+
+export default props =>
+    <div>
+        {/* <Nome nome="Alexandre " sobrenome="Gomes" /> */}
+        <Nome nome="José " sobrenome="Gomes" />
+        <Nome nome="Sérgio " sobrenome="Gomes" />
+        <Nome nome="Maria " sobrenome="Gomes" />
+        <Nome nome="Renata " sobrenome="Gomes" />
+        <Nome nome="Carla " sobrenome="Gomes" />
+    </div>
--- topicos-especiais/src/index.js
+++ topicos-especiais/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Componente1 from './componentes/Componente1';
+// import Componente1 from './componentes/Componente1';
+// import {CompA, CompB as B} from './componentes/Componente2';
+// import  { Teste1, Teste2, Teste3 } from './componentes/MultiElementos';
+// import Familia from './componentes/Familia';
+// import FamiliaGomes from './componentes/FamiliaGomes';
+// import Nome from './componentes/Nome';
+// import ComponenteComFuncao from './componentes/ComponenteComFuncao';
+import Pai from './componentes/Pai';
 
 const element = document.getElementById('root');
 
 ReactDOM.render(
     <div>
-        <Componente1 />
+        {/* <Componente1 /> */}
+        {/* <CompA valor = 'Primeiro Componente'/>
+        <B valor = 'Segundo Componente'/> */}
+        {/* <Teste1 />
+        <Teste2 />
+        <Teste3 /> */}
+        {/* <FamiliaGomes /> */}
+        {/* <Familia sobrenome= 'Gomes' endereco= 'Rua Primeira' numero='nº 234' cidade='Franca' estado='SP'>
+            <Nome nome= 'João'></Nome>
+            <Nome nome = 'Alexandre'></Nome>
+            <Nome nome = 'Joana'></Nome>
+        </Familia> */}
+        {/* <FamiliaGomes sobrenome='Gomes'/> */}
+        {/* <ComponenteComFuncao/> */}
+        <Pai/>
     </div>
     , element
 )
